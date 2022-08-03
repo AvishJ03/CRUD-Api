4a0a5a1690998d8dfe9e5f071b089f2a9a5254f9
Avish Jain <avishjain0@gmail.com>
2022-08-03 17:12:04 +0530

    id

--- app/controllers.js
+++ app/controllers.js
@@ -1,4 +1,3 @@
-
 exports.createUser = async (req, res) => {
   console.log(req.body);
   console.log(req.params);
@@ -15,16 +14,19 @@ exports.getUserById = async (req, res) => {
   console.log(req.body);
   console.log(req.params);
   console.log(req.query);
+  console.log(req.params.id);
 };
 
 exports.updateUser = async (req, res) => {
   console.log(req.body);
   console.log(req.params);
   console.log(req.query);
+  console.log(req.params.id);
 };
 
 exports.deleteUser = async (req, res) => {
   console.log(req.body);
   console.log(req.params);
   console.log(req.query);
+  console.log(req.params.id);
 };
